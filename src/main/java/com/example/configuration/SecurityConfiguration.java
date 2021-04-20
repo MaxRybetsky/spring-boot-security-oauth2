2467c57d32cf87e865a984d328c345613b00d62d
MaxRybetsky <stsdewar@ya.ru>
2021-04-20 19:18:10 +0300

    Add Google authentication and Custom error page.

--- src/main/java/com/example/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/configuration/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
@@ -13,10 +14,10 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        SimpleUrlAuthenticationFailureHandler handler = new SimpleUrlAuthenticationFailureHandler("/");
         http
                 .csrf(
-                        c -> c
-                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 )
                 .authorizeRequests(
                         a -> a
@@ -24,16 +25,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                                 .anyRequest().authenticated()
                 )
                 .exceptionHandling(
-                        e -> e
-                                .authenticationEntryPoint(
-                                        new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)
-                                )
+                        e -> e.authenticationEntryPoint(
+                                new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)
+                        )
                 )
-                .oauth2Login()
-                .and()
-                .logout(
-                        l -> l
-                                .logoutSuccessUrl("/").permitAll()
-                );
+                .oauth2Login(
+                        o -> o.failureHandler(
+                                (request, response, exception) -> {
+                                    request.getSession().setAttribute("error.message", exception.getMessage());
+                                    handler.onAuthenticationFailure(request, response, exception);
+                                }
+                        ))
+                .logout(l -> l.logoutSuccessUrl("/").permitAll());
     }
 }
